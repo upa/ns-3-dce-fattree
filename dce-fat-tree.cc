37778dcbe3bdd94cdcf55c45a7d7bf0b271a627b
Ryo Nakamura <upa@haeena.net>
2014-10-12 05:46:06 +0900

    routes can be installed. But ping test failed.

--- dce-fat-tree.cc
+++ dce-fat-tree.cc
@@ -70,7 +70,7 @@ RunPing(Ptr<Node> node, Time at, const char *target)
 {
 	std::ostringstream oss;
 
-	oss << "-w 1 -c 1 " << target;
+	oss << "-c 2 " << target;
 
 	DceApplicationHelper process;
 	ApplicationContainer apps;
@@ -177,31 +177,31 @@ main (int argc, char ** argv)
 		simaddr2 << root + 1 << "." << pod + 1 << "." 
 			 << aggr + 1 << "." << "2/24";
 
-		AddAddress(nc_root2aggr[linkn].Get(0), Seconds(1),
+		AddAddress(nc_root2aggr[linkn].Get(0), Seconds(0.1),
 			   ndc_root2aggr[linkn].Get(0)->GetIfIndex(),
 			   simaddr1.str().c_str());
-		AddAddress(nc_root2aggr[linkn].Get(1), Seconds(1),
+		AddAddress(nc_root2aggr[linkn].Get(1), Seconds(0.1),
 			   ndc_root2aggr[linkn].Get(1)->GetIfIndex(),
 			   simaddr2.str().c_str());
 
 		RunIp(nc_root2aggr[linkn].Get(0), Seconds(0.11), simup1.str());
-		RunIp(nc_root2aggr[linkn].Get(1), Seconds(0.11), simup1.str());
+		RunIp(nc_root2aggr[linkn].Get(1), Seconds(0.11), simup2.str());
 
 
 		/* set route from Root to Aggr prefix is Pod+1+200.0.0.0/8 */
 		std::stringstream podprefix, aggrsim;
 		podprefix << pod + 1 + 200 << ".0.0.0/8";
 		aggrsim << root + 1 << "." << pod + 1 << "." 
-			 << aggr + 1 << "." << "2";
-		AddRoute(nc_root2aggr[linkn].Get(0), Seconds(3),
+			<< aggr + 1 << "." << "2";
+		AddRoute(nc_root2aggr[linkn].Get(0), Seconds(0.12),
 			 podprefix.str().c_str(), aggrsim.str().c_str());
 
-		/* set route from Aggr to Root Loopback*/
+		/* set route from Aggr to Root Loopback */
 		std::stringstream rootlo, rootsim;
-		rootlo << ROOTLOPREFIX << root + 1 << "";
+		rootlo << ROOTLOPREFIX << root + 1 << "/32";
 		rootsim << root + 1 << "." << pod + 1 << "." 
 			<< aggr + 1 << "." << "1";
-		AddRoute(nc_root2aggr[linkn].Get(1), Seconds(3),
+		AddRoute(nc_root2aggr[linkn].Get(1), Seconds(0.12),
 			 rootlo.str().c_str(), rootsim.str().c_str());
 
 	}
@@ -256,7 +256,7 @@ main (int argc, char ** argv)
 		edgeprefix << pod + 1 + 200 << "." << edge + 1 << ".0.0/16";
 		edgesim << pod + 1 + 100 << "." << aggr + 1 << "."
 			<< edge + 1 << "." << "2";
-		AddRoute(nc_aggr2edge[linkn].Get(0), Seconds(3.1),
+		AddRoute(nc_aggr2edge[linkn].Get(0), Seconds(0.22),
 			 edgeprefix.str().c_str(), edgesim.str().c_str());
 
 		/* set route from Edge to Aggr, 
@@ -265,10 +265,11 @@ main (int argc, char ** argv)
 		 */
 		for (int rootn = 0; rootn < KARY2; rootn++) {
 			std::stringstream rootlo, aggrsim;
-			rootlo << ROOTLOPREFIX << aggr * KARY2 + rootn + 1;
+			rootlo << ROOTLOPREFIX <<
+				aggr * KARY2 + rootn + 1 << "/32";
 			aggrsim << pod + 1 + 100 << "." << aggr + 1 << "."
 				<< edge + 1 << "." << "1";
-			AddRoute(nc_aggr2edge[linkn].Get(1), Seconds(3.1),
+			AddRoute(nc_aggr2edge[linkn].Get(1), Seconds(0.22),
 				 rootlo.str().c_str(), aggrsim.str().c_str());
 		}
 	}
@@ -324,7 +325,7 @@ main (int argc, char ** argv)
 		droute << "0.0.0.0/0";
 		edgesim << pod + 1 + 200 << "." << edge + 1 << "."
 			<< node + 1 << "." << "1";
-		AddRoute(nc_edge2node[linkn].Get(1), Seconds(3.2),
+		AddRoute(nc_edge2node[linkn].Get(1), Seconds(0.32),
 			 droute.str().c_str(), edgesim.str().c_str());
 	}
 	}
@@ -336,11 +337,13 @@ main (int argc, char ** argv)
 	 */
 
 	for (int root = 0; root < ROOTSWNUM; root++) {
-		std::stringstream loaddr;
+		std::stringstream loaddr, loup;
 
+		loup << "link set lo up";
 		loaddr << ROOTLOPREFIX << root + 1 << "/32";
 		AddLoAddress(rootsw.Get(root), Seconds(0.4),
 			     loaddr.str().c_str());
+		RunIp(rootsw.Get(root), Seconds(0.41), loup.str().c_str());
 	}
 
 	/* set up loopback address of aggregation switches.
@@ -349,12 +352,14 @@ main (int argc, char ** argv)
 
 	for (int pod = 0; pod < PODNUM; pod++) {
 	for (int aggr = 0; aggr < AGGRSWINPODNUM; aggr++) {
-		std::stringstream loaddr;
-
+		std::stringstream loaddr, loup;
 		int aggrn = AGGRSWINPODNUM * pod + aggr;
+
+		loup << "link set lo up";
 		loaddr << AGGRLOPREFIX << pod + 1 << "." << aggr + 1 << "/32";
 		AddLoAddress(aggrsw.Get(aggrn), Seconds(0.4),
 			     loaddr.str().c_str());
+		RunIp(aggrsw.Get(aggr), Seconds(0.41), loup.str().c_str());
 	}
 	}
 
@@ -381,16 +386,17 @@ main (int argc, char ** argv)
 	}
 
 	/* ping test */
-	int time = 10;
+	float time = 0.5;
 	for (int root = 0; root < ROOTSWNUM; root++) {
 		std::stringstream rootlo;
 		rootlo << ROOTLOPREFIX << root + 1;
-		RunPing(nodes.Get(0), Seconds(time++),
+		RunPing(nodes.Get(0), Seconds(6.0 + time),
 			rootlo.str().c_str());
+		time += 2.0;
 	}
 
 
-	int stoptime = 30 + NODENUM * ROOTSWNUM;
+	int stoptime = 120;
 
 	if (stoptime != 0) {
 		Simulator::Stop(Seconds(stoptime));
